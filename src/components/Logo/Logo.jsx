85970000ec5f60f1d272d12beeb454d54d710a61
VIgneshwaran Balamurugan <pvjb187@gmail.com>
2020-05-08 17:49:19 +0530

    image link actions

---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Tilt from 'react-tilt';
+import './Logo.css';
+import vision from './eye.png';
+
+const Logo = () => {
+    return (
+        <div className="ma4 mt0">
+            <Tilt
+                className="Tilt br2 shadow-2"
+                options={{ max: 25 }}
+                style={{ height: 100, width: 100 }}
+            >
+                <div className="Tilt-inner">
+                    <img className="tc" src={vision} alt="" />
+                </div>
+            </Tilt>
+        </div>
+    )
+}
+
+export default Logo
