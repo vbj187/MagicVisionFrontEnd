a828d9d9a3789d081532d31929f504e534bb3898
VIgneshwaran Balamurugan <pvjb187@gmail.com>
2020-05-09 16:39:06 +0530

    magic vision thumbnail

--- src/components/FaceRecognition/FaceRecognition.jsx
+++ src/components/FaceRecognition/FaceRecognition.jsx
@@ -6,7 +6,7 @@ function FaceRecognition({ imageUrl, box }) {
     return (
         <div className="center ma">
             <div className="absolute mt2">
-                <img id='inputimage' src={imageUrl} alt="" width='500px' height='auto' />
+                <img id='inputimage' src={imageUrl} alt="" width='auto' height='300px' />
                 <div className="bounding-box"
                     style={{ top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol }}>
                 </div>
